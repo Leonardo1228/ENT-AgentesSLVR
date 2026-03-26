d3aceafe7ab75faf60f48c5ffd00cf55b5b50570
Leonardo1228 <142549969+Leonardo1228@users.noreply.github.com>
2026-03-26 09:58:50 -0500

    Added lifeTime to Food.

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public int nutrition = 5;
+    public float lifetimeSeconds = 5f; // X tiempo de la comida antes de desaparecer
+
+    void Start()
+    {
+        Destroy(gameObject, lifetimeSeconds);
+    }
 }
